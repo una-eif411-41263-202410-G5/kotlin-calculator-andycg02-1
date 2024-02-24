1cda13f4fa5c3c4126c7fffb183656c966c53651
andycg02 <127338006+andycg02@users.noreply.github.com>
2024-02-23 19:14:29 -0600

    Update Calculadora.kt
    Se hizo las funciones de la clase calculadora

--- src/main/kotlin/design/patterns/calculadora/Calculadora.kt
+++ src/main/kotlin/design/patterns/calculadora/Calculadora.kt
@@ -1,2 +1,28 @@
 package design.patterns.design.patterns.calculadora
 
+class Calculadora{
+
+    //Suma
+    fun sumar(a: Int, b: Int): Int{
+        return a + b
+    }
+
+    //Resta
+    fun restar(a: Int, b: Int): Int{
+        return a - b
+    }
+
+    //Multiplicacion
+    fun multiplicar(a: Int, b: Int): Int{
+        return a * b
+    }
+
+    //Dividir
+    fun division(a:Int, b:Int):Int{
+        if (b == 0) {
+            throw IllegalArgumentException("No se puede dividir por cero")
+        }
+        return a / b
+    }
+
+}
